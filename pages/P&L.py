ac184acba174934b9b74bd168282b0bdec189a76
kshitizkamra <68267771+kshitizkamra@users.noreply.github.com>
2025-02-04 18:02:19 +0530

    Update P&L.py

--- pages/P&L.py
+++ pages/P&L.py
@@ -36,7 +36,7 @@ count=0
 st.markdown("""
     <style>
             .block-container {
-                padding-top: 5rem;
+                padding-top: 0rem;
                 padding-bottom: 0rem;
                 padding-left: 1rem;
                 padding-right: 1rem;
