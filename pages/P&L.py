ed74e712c159ec9f4f43a765a6f5de25cd716e5e
kshitizkamra <68267771+kshitizkamra@users.noreply.github.com>
2025-04-01 16:48:54 +0530

    Update P&L.py

--- pages/P&L.py
+++ pages/P&L.py
@@ -191,7 +191,7 @@ except:
         db_data=db_data
         db_sales_data=db_sales_data  
               
-st.title ("P&L Overview")
+st.title ("💸 P&L Overview")
         
 db_data_final=db_data
 db_sales_data_final=db_sales_data
